87688b6472fd77d89360e12a5497045ed9a263a7
Michael C <michael@mchang.name>
2021-07-08 15:16:30 -0400

    update dependencies

--- bot.js
+++ bot.js
@@ -12,11 +12,11 @@ client.on('message', message => {
   if (!message.author.bot && message.content.startsWith(prefix)) { // check if sent by bot & check for prefix
     const text = message.content.slice(prefix.length)
     message.channel.send(owoify(text))
-}
+  }
 })
 
-function owoify(text) {
-    text = text.replace(/(?:r|l)/g, 'w')
+const owoify = (text) => 
+  text.replace(/(?:r|l)/g, 'w')
     .replace(/(?:R|L)/g, 'W')
     // n[aeiou] with ny[aeiou]
     .replace(/n([aeiou])/g, 'ny$1')
@@ -24,5 +24,3 @@ function owoify(text) {
     .replace(/N([AEIOU])/g, 'Ny$1')
     // ove with uv
     .replace(/ove/g, 'uv')
-  return text
-}
